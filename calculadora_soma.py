51e5bab2b3e6b89c3af71c47e7808f0b56d66e10
Matheus <matheus_benini@hotmail.com>
2025-06-18 09:46:45 -0300

    altera calculadora soma para ler valores

--- calculadora_soma.py
+++ calculadora_soma.py
@@ -1,4 +1,4 @@
-numero1 = 12
-numero2 = 14
-
-print("A soma de", numero1, "e", numero2, "é:", numero1 + numero2)
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é: {soma}")
